74978252089f00780a835267ae2868546471ff2b
k3kc <x913x913@gmail.com>
2017-04-05 11:15:23 +0500

    Added MVC support

--- src/LanguageFeatures/Startup.cs
+++ src/LanguageFeatures/Startup.cs
@@ -16,13 +16,14 @@ namespace LanguageFeatures
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
-
+            app.UseMvcWithDefaultRoute();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
