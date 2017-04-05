e0dce9758e67c2f9a1e78feec40854be209f17ae
k3kc <x913x913@gmail.com>
2017-04-05 11:19:27 +0500

    Added Product model

---
+++ src/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            //Product kayak = new Product() {  };
+
+            return new Product[] {
+                new Product() { Name = "Kayak", Price = 275M },
+                new Product() { Name = "LifeJacket", Price = 48.55M }
+            };
+        }
+
+    }
+}
